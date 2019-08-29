c9bfa015bf91f5e2a26c2e3da517bbe7a09fe4ae
i330z <izzazahmed134@gmail.com>
2019-08-29 16:41:30 +0545

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const modelParams = {
     imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
     maxNumBoxes: 20,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.79,    // confidence threshold for predictions.
+    scoreThreshold: 0.89,    // confidence threshold for predictions.
   }
 
   navigator.getUserMedia =  navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -19,7 +19,7 @@ const modelParams = {
           navigator.getUserMedia({video:{}}, stream =>{
               video.srcObject = stream;
               
-              setInterval(runDetection,100)
+              setInterval(runDetection,300)
           },
           err => console.log(err));
       }
@@ -32,7 +32,7 @@ const modelParams = {
               let hand1 = predictions[0].bbox;
               let x = hand1[0];
               let y = hand1[1];
-            
+                console.log(x,y)
               if(y > 300){
                 if(x < 200){
                     audio.src = 'a-chord.mp3';
